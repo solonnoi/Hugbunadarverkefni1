10e2b7f3576ebf05234ff5eb6173d7c467fbacd9
kolbrunjulia <kjg18@hi.is>
2022-08-31 21:53:26 +0000

    Initial commit

---
+++ src/main/java/is/hi/hbv501g/hbv501g/Hbv501GApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.hbv501g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hbv501GApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hbv501GApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g/hbv501g/Hbv501GApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.hbv501g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hbv501GApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
