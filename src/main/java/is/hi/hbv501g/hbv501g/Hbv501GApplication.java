a072864ace251591a6a8590900449281cf33b70d
solonnoi <55716697+solonnoi@users.noreply.github.com>
2022-08-31 22:22:32 +0000

    the first commit

--- src/main/java/is/hi/hbv501g/hbv501g/Hbv501GApplication.java
+++ src/main/java/is/hi/hbv501g/hbv501g/Hbv501GApplication.java
@@ -2,12 +2,14 @@ package is.hi.hbv501g.hbv501g;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Halló, þetta er sólon
 
 @SpringBootApplication
 public class Hbv501GApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Hbv501GApplication.class, args);
+
     }
 
 }
