16ae59d9f597d9e0ace840194483e04c20111915
solonnoi <55716697+solonnoi@users.noreply.github.com>
2022-09-01 14:34:03 +0000

    Demo application frá https://spring.io/quickstart, virkar ekki :(

--- src/main/java/is/hi/hbv501g/hbv501g/Hbv501GApplication.java
+++ src/main/java/is/hi/hbv501g/hbv501g/Hbv501GApplication.java
@@ -2,14 +2,19 @@ package is.hi.hbv501g.hbv501g;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 // Halló, þetta er sólon
 
 @SpringBootApplication
+@RestController
 public class Hbv501GApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Hbv501GApplication.class, args);
-
     }
-
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
