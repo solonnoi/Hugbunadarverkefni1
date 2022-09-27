783c9a15bc97a040fe4cd986a5da2051d0829935
solonnoi <55716697+solonnoi@users.noreply.github.com>
2022-09-27 15:30:19 +0200

    Nær ekki að initialize-a JPA entity manager....

--- src/main/java/is/hi/hbv501g/hbv501g/Hbv501GApplication.java
+++ src/main/java/is/hi/hbv501g/hbv501g/Hbv501GApplication.java
@@ -15,15 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 // ATH er ekki viss um að maður eigi að láta þetta virka svona en núna ef maður setur inn
 // Url-ið "http://localhost:8080/yeahBuddy" og keyrir þá ætti e-ð að koma.
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@RestController
+@SpringBootApplication
 public class Hbv501GApplication {
     public static void main(String[] args) {
         SpringApplication.run(Hbv501GApplication.class, args);
     }
-    @GetMapping("/yeahBuddy")
-    public String hello(@RequestParam(value = "yeah", defaultValue = "Buddy") String name) {
-        return String.format("Hello %s!", name);
     }
 
-}
+
